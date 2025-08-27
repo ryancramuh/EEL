51d97384862332bfed1da99dc311b15ce0d958cb
ryancramuh <ryancramer22@gmail.com>
2025-08-27 14:17:39 -0700

    HAZ Unit needs work

--- rtl/combinational/HAZARD.sv
+++ rtl/combinational/HAZARD.sv
@@ -18,28 +18,8 @@ module HAZARD (
     output logic FWD_MEM_SEL
 );
 
-    logic FWD_EX1       = ((D_ADDR1 == E_WADDR) && (!E_MEM_WRITE) && (!M_MEM_WRITE));
-    logic FWD_EX2       = ((D_ADDR2 == E_WADDR) && (!E_MEM_WRITE) && (!M_MEM_WRITE));
-    logic FWD_MEM1
-    logic FWD_MEM2
-    logic FWD_WB1
-    logic FWD_WB2
-    logic STALL1_EX_DEP = 
-    logic STALL2_EX_DEP = 
-    logic STALL1_MEM_DEP = 
-    logic STALL2_MEM_DEP = 
+    always_comb
 
     always_comb begin
-        
-        if((M_WADDR == D_ADDR1) || (M_WADDR == D_ADDR2)) begin
-            if(M_WADDR == D_ADDR1) begin
-                FWD_A_SEL = 2'b11; // wdata
-            end
-            else begin
-                FWD_B_SEL = 2'b11; // wdata
-            end
-        end
 
-        if(())
-    end
 endmodule
